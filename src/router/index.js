2a8c7c36fdc7f4b791af3ebc107d098e08878ffc
xiumubai <1547702880@qq.com>
2022-08-15 16:45:23 +0800

    feat: 修改search组件

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,18 @@
  * @Author: 朽木白
  * @Date: 2022-08-10 11:11:04
  * @LastEditors: 1547702880@qq.com
- * @LastEditTime: 2022-08-10 16:01:55
+ * @LastEditTime: 2022-08-15 16:37:22
  * @Description:
  */
 import { createRouter, createWebHistory } from 'vue-router';
-import Search from '../views/Search.vue';
+import SearchList from '../views/SearchList.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'search',
-      component: Search,
+      component: SearchList,
     },
     {
       path: '/goodsList',
