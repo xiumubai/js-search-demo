a0be3b1cd7ae494376b82ce4d693462e16840cbb
xiumubai <1547702880@qq.com>
2022-08-15 11:40:46 +0800

    feat: 配置less

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+/*
+ * @Author: 朽木白
+ * @Date: 2022-08-10 11:11:04
+ * @LastEditors: 1547702880@qq.com
+ * @LastEditTime: 2022-08-15 11:37:30
+ * @Description:
+ */
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        // modifyVars: generateModifyVars(),
+        javascriptEnabled: true,
+      },
+    },
+  },
+});
